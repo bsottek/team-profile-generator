6c35cc56597ce63a498cc92e7e53aed01b76e2d3
bsottek <bsottek@gmail.com>
2021-06-28 15:50:02 -0500

    Employee methods and passing tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,9 +3,34 @@ const { expect } = require('@jest/globals');
 
 test('creates an employee object', () => {
     const employee = new Employee('Ben');
-    console.log(employee);
+    console.table(employee);
 
     expect(employee.name).toBe('Ben');
     expect(employee.id).toBeDefined();
     expect(employee.email).toBe(`${employee.name}@company.com`);
+    expect(employee.role).toBe('Employee');
+})
+
+test('gets employee name', () => {
+    const employee = new Employee('Ben');
+
+    expect(employee.getName()).toEqual(expect.stringContaining('Ben'));
+})
+
+test('gets employee id', () => {
+    const employee = new Employee('Ben');
+
+    expect(employee.getId()).toBe(employee.id);
+})
+
+test('gets employee email', () => {
+    const employee = new Employee('Ben');
+
+    expect(employee.getEmail()).toBe(employee.email);
+})
+
+test('gets employee role', () => {
+    const employee = new Employee('Ben');
+
+    expect(employee.getRole()).toBe(employee.role);
 })
