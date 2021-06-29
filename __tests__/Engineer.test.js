82998ff10c5947c1516e407e287033af9e2f982e
bsottek <bsottek@gmail.com>
2021-06-29 14:06:36 -0500

    Engineer class constructor and passing tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -12,3 +12,16 @@ test('create engineer object', () => {
     expect(engineer.github).toBeDefined();
     expect(engineer.role).toBe('Engineer');
 })
+
+test('return github username', () => {
+    const engineer = new Engineer('Ben', 'ben@company.com', 'bsottek', 1234);
+
+    expect(engineer.getGithub()).toBe('bsottek');
+})
+
+test('return role (engineer)', () => {
+    const engineer = new Engineer('Ben', 'ben@company.com', 'bsottek', 1234);
+    console.log(engineer.getRole());
+
+    expect(engineer.getRole()).toBe('Engineer');
+})
